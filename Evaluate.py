3b94b8638d051e646a9c7d096c08cbbd555b06de
NIRMAL KUMAR <nirmalkr805@gmail.com>
2024-02-02 01:08:07 +0530

    Evaluates arithmetic expressions
    Evaluates arithmetic expressions from a file and writes answers into another file.

---
+++ Evaluate.py
@@ -0,0 +1,10 @@
+#Challenge 2 – Evaluate Arithmetic Expressions
+
+with open('file.txt', 'r') as input_file, open('output.txt', 'w') as output_file:
+    for line in input_file:
+        expression = line.strip()
+        expression = expression.replace("=",'')
+        expression = expression.replace("^",'**')
+        expression = expression.replace(") (",') * (')
+        result = eval(expression)
+        output_file.write(f"{expression}= {result}\n")
